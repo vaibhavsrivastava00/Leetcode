9f4660ebcca04886d394efef5c36267941c36d79
vaibhavsrivastava00 <vaibhav.srivastava.291@gmail.com>
2025-01-04 22:34:07 +0530

    changed sometihng

--- Array/Easy level questions/283.MoveZeros.cpp
+++ Array/Easy level questions/283.MoveZeros.cpp
@@ -7,6 +7,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int left = 0;
 
+
+
+
+
+
         for (int right = 0; right < nums.size(); right++) {
             if (nums[right] != 0) {
                 swap(nums[right], nums[left]);
