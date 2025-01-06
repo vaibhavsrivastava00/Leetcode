3025ea49e4a49c9ef520c473173037b24f4daab0
vaibhavsrivastava00 <vaibhav.srivastava.291@gmail.com>
2025-01-06 22:45:15 +0530

    changes

--- Array/Easy level questions/283.MoveZeros.cpp
+++ Array/Easy level questions/283.MoveZeros.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <vector>
-// comment
+// new
 using namespace std;
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int left = 0;
-
-
-
-
-
 
         for (int right = 0; right < nums.size(); right++) {
             if (nums[right] != 0) {
