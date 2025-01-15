c841f5eb487520f50f102319c9cbde0242e1bb89
vaibhavsrivastava00 <vaibhav.srivastava.291@gmail.com>
2025-01-15 22:44:19 +0530

    optimised 543

--- Tree/543_Height_of_a_Binary_Tree.cpp
+++ Tree/543_Height_of_a_Binary_Tree.cpp
@@ -1,3 +1,4 @@
+//brute force
 class Solution {
 public:
     int height(TreeNode* root){
@@ -14,5 +15,32 @@ public:
         int opt3 = height(root->left) + height(root->right) ;
         int ans = max(opt1,max(opt2,opt3));
         return ans;
+    }
+};
+//optimised approch
+class Solution {
+public:
+    pair<int,int> diameter(TreeNode* root){
+        if(root == NULL){
+            pair<int,int>p = make_pair(0,0);
+            return p;
+        }
+        pair<int,int>left = diameter(root->left);
+        pair<int,int>right = diameter(root->right);
+
+        int opt1 = left.first;
+        int opt2 = right.first;
+        int opt3 = left.second+right.second;
+
+        pair<int,int>ans;
+        ans.first = max(opt1,max(opt2,opt3));
+        ans.second = max(left.second,right.second)+1;
+        return ans;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        pair<int,int>ans;
+        ans = diameter(root);
+        
+        return ans.first;
     }
 };
